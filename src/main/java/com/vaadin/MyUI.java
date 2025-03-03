76264db8b5f1d601d6e17837c5d318f232a3d6ea
Onur Idrisoglu <onur@vaadin.com>
2025-03-03 12:35:40 +0200

    Initial commit

---
+++ src/main/java/com/vaadin/MyUI.java
@@ -0,0 +1,63 @@
+package com.vaadin;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Item;
+import com.vaadin.data.util.IndexedContainer;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.*;
+import com.vaadin.ui.renderers.ButtonRenderer;
+import com.vaadin.ui.renderers.Renderer;
+import org.apache.commons.collections.MapUtils;
+import org.vaadin.grid.cellrenderers.editable.SimpleSelectRenderer;
+import org.vaadin.grid.cellrenderers.editable.TextFieldRenderer;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class MyUI extends UI {
+
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+        final VerticalLayout layout = new VerticalLayout();
+
+        Grid grid = new Grid();
+        ButtonRenderer buttonRenderer = new ButtonRenderer(evt -> Notification.show("clicked"));
+        grid.addColumn("name").setRenderer(buttonRenderer);
+        grid.addColumn("surname");
+
+        for(int i = 0; i < 10; i++) {
+            grid.addRow("Name " + i, "Surname " + i);
+        }
+        
+        grid.setReadOnly(true);
+        grid.addSelectionListener(evt -> Notification.show("Selected a person"));
+
+        layout.addComponents(grid);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        layout.setHeight("100%");
+
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
---
+++ src/main/java/com/vaadin/Person.java
@@ -0,0 +1,31 @@
+package com.vaadin;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
